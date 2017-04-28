0482f2c8c335c949d86c66d46d004437d78ac717
Antioch Sanders <antiochsanders@gmail.com>
2017-04-28 13:11:21 -0400

    Two Players, Death

--- Sneks/client/script.js
+++ Sneks/client/script.js
@@ -1,3 +1,4 @@
+// draw board variables
 var yOffset = 3;
 var xOffset = 4;
 var ySquares = 27;
@@ -6,10 +7,35 @@ var squareSize = 22;
 var colorOne = 'rgb(180, 180, 180)';
 var colorTwo = 'rgb(200, 200, 200)';
 var snekSquareSize = squareSize - 6;
-var snake = new Snek();
-var controlKeys = [' ', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
-controlKeys = new Set(controlKeys);
 
+// controls
+var DIRS = Object.freeze({
+    UP : 'ArrowUp',
+    DOWN : 'ArrowDown',
+    LEFT : 'ArrowLeft',
+    RIGHT : 'ArrowRight',
+    set : new Set(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'])
+});
+
+var IO_EVTS = Object.freeze({
+    KEY_DN : 'key down',
+    SNAKE_DATA : 'snake data',
+    STATE : "game state"
+});
+
+var GAME_STATE = Object.freeze({
+    START : 'start',
+    PLAY : 'play',
+    OVER : 'over'
+});
+
+
+// player snakes
+var snake = new Snek("black");
+var opponent = new Snek("white");
+
+// game starte
+var state = GAME_STATE.PLAY;
 
 function drawBoard(ctx, colorOne, colorTwo) {
     var color = colorOne;
@@ -30,9 +56,9 @@ function drawSquare(ctx, x, y, s) {
                     yOffset + squareSize * y + (squareSize - s) / 2, s, s);
 };
 
-function Snek() {
+function Snek(color) {
     this.squares = [[0, 0], [1, 0], [2, 0], [3, 0]];
-    this.color = "red";
+    this.color = color;
     this.draw = function(ctx) {
         ctx.fillStyle = this.color;
         for (var i = 0; i < this.squares.length; i++) {
@@ -44,12 +70,16 @@ function Snek() {
 
 function initIO() {
     socket = io();
-    socket.on('snake data', function(data) {
-        snake.squares = data;
+    socket.on(IO_EVTS.SNAKE_DATA, function(data) {
+        snake.squares = data[0];
+        opponent.squares = data[1];
+    });
+    socket.on(IO_EVTS.STATE, function(data) {
+        state = data;
     });
     $('#game-canvas').keydown(function(event) {
-        if (controlKeys.has(event.key)) {
-            socket.emit('key down', event.key);
+        if (DIRS.set.has(event.key)) {
+            socket.emit(IO_EVTS.KEY_DN, event.key);
         }
     });
 };
@@ -57,10 +87,22 @@ function initIO() {
 $("document").ready(function() {
     var canvas = document.getElementById("game-canvas");
     canvas.focus();
+
     var ctx = canvas.getContext('2d');
     setInterval(function() {
-        drawBoard(ctx, colorOne, colorTwo);
-        snake.draw(ctx);
+        if (state == GAME_STATE.PLAY) {
+            drawBoard(ctx, colorOne, colorTwo);
+            snake.draw(ctx);
+            opponent.draw(ctx);
+        } else if (state == GAME_STATE.OVER) {
+            ctx.fillStyle = "rgb(70, 70, 70)";
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.textAlign = "center";
+            ctx.fillStyle = "white";
+            ctx.font = '60px Arial';
+            ctx.fillText("Game Over D:", canvas.width / 2, canvas.height / 2);
+        }
     }, 50);
     initIO();
 });
+
--- Sneks/index.js
+++ Sneks/index.js
@@ -2,23 +2,91 @@
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
-var snek = require("socket.io")(http);
+var snek = require('socket.io')(http);
 
 // static files
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/client/play.html");
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/play.html');
 });
 
-// handle clients
-snek.on('connection', function(socket) {
-    var snake = new Snake();
-    setInterval(function() {
-        snake.move(true);
-        socket.emit('snake data', snake.squares);
-    }, 50);
-    socket.on('key down', function(data) {
-        snake.tryTurn(data);
+// enums
+
+var DIRS = Object.freeze({
+    UP : 'ArrowUp',
+    DOWN : 'ArrowDown',
+    LEFT : 'ArrowLeft',
+    RIGHT : 'ArrowRight',
+    set : new Set(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'])
+});
+
+var IO_EVTS = Object.freeze({
+    KEY_DN : 'key down',
+    SNAKE_DATA : 'snake data',
+    STATE : "game state"
+});
+
+var GAME_STATE = Object.freeze({
+    OVER : 'over'
+});
+
+
+var queue = [];
+var games = new Set();
+
+var Game = function(first, second) {
+    console.log('Paired Off:', first['id'], second['id']);
+
+    // init snakes with squares
+    var snakeOneSquares = [];
+    for (var x = 0; x <= 7; x++) {
+        snakeOneSquares.push([x, 0]);
+    }
+    var snakeOne = new Snake(snakeOneSquares, DIRS.RIGHT);
+    var snakeTwoSquares = []
+    for (var x = 0; x <= 7; x++) {
+        snakeTwoSquares.push([xSquares - 1 - x, ySquares - 1]);
+    }
+    var snakeTwo = new Snake(snakeTwoSquares, DIRS.LEFT);
+
+    // listen for input
+    first.on(IO_EVTS.KEY_DN, function(data) {
+        snakeOne.tryTurn(data);
     });
+    second.on(IO_EVTS.KEY_DN, function(data) {
+        snakeTwo.tryTurn(data);
+    });
+
+    // update timer
+    var interval = setInterval(function() {
+        snakeOne.move(false);
+        snakeTwo.move(false);
+
+        var sqOne = snakeOne.squares;
+        var sqTwo = snakeTwo.squares;
+        first.emit(IO_EVTS.SNAKE_DATA, [sqOne, sqTwo]);
+        second.emit(IO_EVTS.SNAKE_DATA, [sqTwo, sqOne]);
+
+        var oneDead = snakeOne.hitEdge || snakeOne.hitSelf() || snakeOne.hitSnake(snakeTwo);
+        var twoDead = snakeTwo.hitEdge || snakeTwo.hitSelf() || snakeTwo.hitSnake(snakeOne);
+
+        if (oneDead || twoDead) {
+            first.emit(IO_EVTS.STATE, GAME_STATE.OVER);
+            second.emit(IO_EVTS.STATE, GAME_STATE.OVER);
+            clearInterval(interval);
+            games.delete(this);
+        }
+    }, 50);
+}
+
+// pair up clients
+snek.on('connection', function(socket) {
+    if (queue.length) {
+        var pair = queue.shift();
+        var game = new Game(pair, socket);
+        games.add(game);
+    } else {
+        queue.push(socket);
+    }
 });
 
 // boilerplate
@@ -26,34 +94,30 @@ app.use(express.static(__dirname + "/client"))
 http.listen(8000, "127.0.0.1");
 
 // constants
-var directions = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
-directions = new Set(directions);
 var ySquares = 27;
 var xSquares = 36;
 
-
-var Snake = function() {
-    this.squares = [[0, 0], [1, 0], [2, 0], [3, 0],
-        [4, 0], [5, 0], [6, 0], [7, 0]];
- 
-    this.direction = "ArrowRight";
-    this.lastMoved = "ArrowRight";
+var Snake = function(squares, direction) {
+    this.squares = squares; 
+    this.direction = direction;
+    this.lastMoved = direction;
+    this.hitEdge = false;
 
     this.move = function(append) {
-        var last = this.squares[this.squares.length - 1];
-        var newSquare = [last[0], last[1]];
-        if (this.direction == "ArrowUp") {
+        var head = this.squares[this.squares.length - 1];
+        var newSquare = [head[0], head[1]];
+        if (this.direction == DIRS.UP) {
             newSquare[1]--;
-        } else if (this.direction == "ArrowDown") {
+        } else if (this.direction == DIRS.DOWN) {
             newSquare[1]++;
-        } else if (this.direction == "ArrowLeft") {
+        } else if (this.direction == DIRS.LEFT) {
             newSquare[0]--;
         } else {
             newSquare[0]++;
         }
         if (newSquare[0] < 0 || newSquare[1] < 0 ||
              newSquare[0] >= xSquares || newSquare[1] >= ySquares) {
-            return
+            this.hitEdge = true;
         }
         if (!append) {
             this.squares.shift();
@@ -62,12 +126,34 @@ var Snake = function() {
         this.squares.push(newSquare);
     }
 
+    this.hitSelf = function() {
+        var head = this.squares[this.squares.length - 1];
+        for (var i = 0; i < this.squares.length - 1; i++) {
+            var curr = this.squares[i];
+            if (curr[0] == head[0] && curr[1] == head[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    this.hitSnake = function(snake) {
+        var head = this.squares[this.squares.length - 1];
+        for (var i = 0; i < snake.squares.length; i++) {
+            var curr = snake.squares[i];
+            if (curr[0] == head[0] && curr[1] == head[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     this.tryTurn = function(direction) {
-        if (directions.has(direction)) {
-            var valid = direction == "ArrowUp" && this.lastMoved != "ArrowDown";
-            valid = valid || direction == "ArrowDown" && this.lastMoved != "ArrowUp";
-            valid = valid || direction == "ArrowLeft" && this.lastMoved != "ArrowRight";
-            valid = valid || direction == "ArrowRight" && this.lastMoved != "ArrowLeft";
+        if (DIRS.set.has(direction)) {
+            var valid = direction == DIRS.UP && this.lastMoved != DIRS.DOWN;
+            valid = valid || direction == DIRS.DOWN && this.lastMoved != DIRS.UP;
+            valid = valid || direction == DIRS.LEFT && this.lastMoved != DIRS.RIGHT;
+            valid = valid || direction == DIRS.RIGHT && this.lastMoved != DIRS.LEFT;
             if (valid) {
                 this.direction = direction;
             }
